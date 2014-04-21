d20ca8c5fe88b5778fa94e89d470ba074ab8ad95
Onur Aslan <onuraslan@gmail.com>
2014-04-21 12:22:01 +0300

    loadConf at start

--- app/js/app.js
+++ app/js/app.js
@@ -3,11 +3,14 @@ define([
   'underscore',
   'backbone',
   'router',
+  'helpers/conf',
   'views/sidebar',
   'bootstrap',
   'jquery_cookie'
-], function ($, _, Backbone, Router, SidebarView) {
+], function ($, _, Backbone, Router, ConfHelper, SidebarView) {
   var initialize = function () {
+
+    ConfHelper.loadConf ();
 
     var sidebarView = new SidebarView  ();
     sidebarView.render ();
