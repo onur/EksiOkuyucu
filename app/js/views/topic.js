5052d72f37b4cf648f8058d4127f4a7db53e2cca
Onur Aslan <onuraslan@gmail.com>
2014-06-17 14:27:35 +0300

    redirect to DESC order if there is no entry

--- app/js/views/topic.js
+++ app/js/views/topic.js
@@ -71,6 +71,13 @@ define([
                                                      return false; });
           }
 
+          // redirect to DESC order if there is no entry to dipslay
+          if (order == 2 &&
+              entries.length == 0) {
+            location.href = '#t/1/' + that.topicCollection.external_url;
+            return;
+          }
+
           $(that.el).append (_.template (TopicTemplate,
                                          {entries: entries.toJSON ()}));
 
