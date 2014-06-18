8b53c09546d1606eef3b99d260dcff863184729b
Onur Aslan <onuraslan@gmail.com>
2014-06-18 20:48:39 +0300

    add build.js

---
+++ app/js/build.js
@@ -0,0 +1,13 @@
+({
+  baseUrl: ".",
+  paths: {
+    jquery: 'libs/jquery-1.11.0.min',
+    underscore: 'libs/underscore-min',
+    backbone: 'libs/backbone-min',
+    text: 'libs/text',
+    bootstrap: 'libs/bootstrap.min',
+    jquery_cookie: 'libs/jquery.cookie'
+  },
+  name: "main",
+  out: "main-built.js"
+})
