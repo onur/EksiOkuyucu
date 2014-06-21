060e0792645022b99276442f57d6a9c545b28ed4
Onur Aslan <onuraslan@gmail.com>
2014-06-21 15:42:18 +0300

    fill body with body template at start

--- app/js/app.js
+++ app/js/app.js
@@ -5,12 +5,15 @@ define([
   'router',
   'helpers/conf',
   'views/sidebar',
+  'text!templates/body.html',
   'bootstrap',
   'jquery_cookie'
-], function ($, _, Backbone, Router, ConfHelper, SidebarView) {
+], function ($, _, Backbone, Router, ConfHelper, SidebarView, BodyTemplate) {
   var initialize = function () {
 
     ConfHelper.loadConf ();
+
+    $('body').html (_.template (BodyTemplate));
 
     var sidebarView = new SidebarView  ();
     sidebarView.render ();
