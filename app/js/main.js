cb00a6ec9f9e0aecca4ce647ae639e976f6ce2a9
Onur Aslan <onuraslan@gmail.com>
2014-07-03 12:00:45 +0300

    set templates dir in main

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,8 @@ require.config ({
     text: 'libs/text',
     bootstrap: 'libs/bootstrap.min',
     jquery_cookie: 'libs/jquery.cookie',
-    select2: 'libs/select2.min'
+    select2: 'libs/select2.min',
+    templates: '../templates'
   },
 
   shim: {
