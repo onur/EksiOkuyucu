4f5a397614b7f6129aa81faa0033115bdf8e3614
Onur Aslan <onuraslan@gmail.com>
2014-07-05 16:33:21 +0300

    use module based build profile for r.js

--- app/js/build.js
+++ app/js/build.js
@@ -1,17 +1,13 @@
 ({
-  baseUrl: ".",
-  paths: {
-    requireLib: 'libs/require',
-    jquery: 'libs/jquery-1.11.0.min',
-    underscore: 'libs/underscore-min',
-    backbone: 'libs/backbone-min',
-    text: 'libs/text',
-    bootstrap: 'libs/bootstrap.min',
-    jquery_cookie: 'libs/jquery.cookie',
-    select2: 'libs/select2.min',
-    templates: '../templates'
-  },
-  include: 'requireLib',
-  name: "main",
-  out: "main-built.js"
+  appDir: "../",
+  baseUrl: "js",
+  dir: "../../build",
+  mainConfigFile: 'main.js',
+  optimizeCss: 'standard',
+  modules: [
+    {
+      name: "main",
+      include: 'requireLib'
+    }
+  ]
 })
