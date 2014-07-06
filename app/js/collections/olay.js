0bfafbf0fa4274b526374be9e524ecabcd5a9b01
Onur Aslan <onuraslan@gmail.com>
2014-07-06 17:05:24 +0300

    add olay collection

---
+++ app/js/collections/olay.js
@@ -0,0 +1,47 @@
+
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function ($, _, Backbone) {
+  
+  var DebeList = Backbone.Collection.extend ({
+
+    url: 'https://eksisozluk.com/olay',
+
+    parse: function (resp) {
+      var entries = [];
+      var htmlDoc = $.parseHTML (resp);
+      $(htmlDoc).find ('#content ul.topic-list li a').each (function () {
+
+        if ($(this).attr ('class') == 'empty-index-item')
+          return;
+
+        var entry_url = $(this).attr ('href').replace (/\?.*/, '');
+        var entry_snapshot = $(this).attr ('href').replace (/.*snapshot=/, '');
+        // TODO: remove entry count from title
+        var entry_title = $(this).text ();
+        var entry_count = parseInt ($(this).find ('small').text ());
+
+        entries.push ({ url: entry_url,
+                        title: entry_title,
+                        count: entry_count,
+                        snapshot: entry_snapshot });
+      });
+
+      return entries;
+    },
+
+
+    fetch: function (options) {
+      options = options || {};
+      options.dataType = 'html';
+      return Backbone.Collection.prototype.fetch.call (this, options);
+    }
+
+  });
+
+
+  return DebeList;
+
+});
