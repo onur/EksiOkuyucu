fb56a198fa25a5415d2360c900d3e4776c5ddc6f
Onur Aslan <onuraslan@gmail.com>
2014-07-07 14:16:48 +0300

    add copyright information to main module

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
-
-
+/*
+ * EksiOkuyucu - unofficial eksisozluk client
+ * http://eksiokuyucu.com/
+ *
+ * Copyright (C) 2014  Onur Aslan  <onuraslan@gmail.com>
+ * Licensed under MIT
+ * https://github.com/onuraslan/EksiOkuyucu/blob/master/COPYING
+ */
 
 require.config ({
   paths: {
