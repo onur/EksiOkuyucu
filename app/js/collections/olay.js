10b641505d736ac520379cbecc9bca4061e0cae1
Onur Aslan <onuraslan@gmail.com>
2015-03-18 14:02:36 +0200

    remove unread count from title

--- app/js/collections/olay.js
+++ app/js/collections/olay.js
@@ -23,6 +23,11 @@ define([
         var entry_title = $(this).text ();
         var entry_count = parseInt ($(this).find ('small').text ());
 
+        if (entry_count) {
+          var re = new RegExp(entry_count + '$');
+          entry_title = entry_title.replace(re, '');
+        }
+
         entries.push ({ url: entry_url,
                         title: entry_title,
                         count: entry_count ? entry_count : 0,
