898b199af0d365a849576862332f417f2eda451d
Onur Aslan <onuraslan@gmail.com>
2015-04-01 14:40:45 +0300

    add favorite and sukela events

--- app/js/views/entry.js
+++ app/js/views/entry.js
@@ -11,7 +11,9 @@ define([
 
     events: {
       'click div.entry p.content a': 'popover',
-      'click div.entry p.content': 'click'
+      'click div.entry p.content': 'click',
+      'click div.entry a[data-button-type="favorite"]': 'favorite',
+      'click div.entry a[data-button-type="sukela"]': 'sukela',
     },
 
     popover: function (ev) {
@@ -20,6 +22,45 @@ define([
 
     click: function (ev) {
       return PopoverHelper.trigger (ev);
+    },
+
+
+    favorite: function (ev) {
+
+      var favorite_count =
+              parseInt($(ev.currentTarget).attr('data-favorite-count')),
+          is_favorite = 
+              $(ev.currentTarget).attr('data-is-favorite');
+
+      $.ajax ({
+          url: 'https://eksisozluk.com/entry/favori',
+          type: 'POST',
+          data:    { entryId: $(ev.currentTarget).attr('data-id') },
+          headers: { 'X-Requested-With': 'XMLHttpRequest' }
+      });
+
+
+      if (is_favorite === 'false') {
+        favorite_count++;
+        is_favorite = true;
+        $(ev.currentTarget).removeClass('btn-default').addClass('btn-primary');
+      } else {
+        favorite_count--;
+        is_favorite = false;
+        $(ev.currentTarget).removeClass('btn-primary').addClass('btn-default');
+      }
+
+      $(ev.currentTarget).attr('data-favorite-count', favorite_count);
+      $(ev.currentTarget).find('span.favorite-count')
+                         .text(favorite_count ? ' ' + favorite_count : '');
+      $(ev.currentTarget).attr('data-is-favorite', is_favorite);
+
+      return false;
+    },
+
+    sukela: function (ev) {
+
+      return false;
     }
 
   });
