3562305cab88fb32ca648a184bf1c380d90ab960
Onur Aslan <onuraslan@gmail.com>
2015-04-07 11:09:41 +0300

    Load config separately

--- app/js/tests.js
+++ app/js/tests.js
@@ -1,6 +1,22 @@
 
+var specs = [
+  'tests/sidebar',
+  'tests/debe'
+];
 
-require(['config', 'app', 'tests/main'], function (C, App, Tests) {
-  App.initialize(true);
-  Tests.initialize();
+
+require([
+  'config',
+], function () {
+  require([
+    'app',
+    'jasmine-boot',
+    'jasmine-jquery'
+  ], function (App) {
+    App.initialize(true);
+    // Load the specs
+    require(specs, function () {
+      window.onload();
+    });
+  });
 });
