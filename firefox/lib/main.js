de3ff5fd4fccdf5ac4fcf6d6eeeef954f510885b
Onur Aslan <onuraslan@gmail.com>
2015-04-08 14:43:10 +0300

    open index in new tab instead of panel

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -3,29 +3,21 @@ var widgets = require ("sdk/widget");
 var tabs = require ("sdk/tabs");
 var data = require ("sdk/self").data;
 
-var EksiPanel = require ("sdk/panel").Panel ({
-  position: {
-    left: 0,
-    top: 0,
-    right: 0,
-    bottom: 0
-  },
-  contentURL: data.url ("index.html"),
-  contentScriptFile: [
-    data.url ('js/main.js')
-  ]
-});
-
-EksiPanel.on ("show", function (){
-  EksiPanel.port.emit ("show");
-});
-
-
 var widget = widgets.Widget ({
   id: "eksi-okuyucu",
   label: "Eksi Okuyucu",
-  contentURL: require ("sdk/self").data.url ("img/icon.png"),
+  contentURL: data.url ("img/icon.png"),
   onClick: function () {
-    EksiPanel.show ();
+    //EksiPanel.show ();
+    tabs.open({
+      url: data.url("index.html"),
+      inBackground: false,
+      onReady: function(tab)
+      {
+        tab.attach({
+          contentScriptFile: data.url("js/main.js"),
+        });
+      }
+    });
   }
 });
