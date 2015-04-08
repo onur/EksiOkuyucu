227bdead40b0963124a72ec59f10aa12a035a270
Onur Aslan <onuraslan@gmail.com>
2015-04-08 15:16:12 +0300

    fix extension url for chrome

--- chrome/chrome.js
+++ chrome/chrome.js
@@ -6,6 +6,5 @@
 
 // open index.html when user click extension icon
 chrome.browserAction.onClicked.addListener (function (tab) {
-  chrome.tabs.create ({'url': chrome.extension.getURL ('app/index.html')});
-  //window.open(chrome.extension.getURL("app/index.html"),"eksi-okuyucu-window","width=640,height=480")
+  chrome.tabs.create ({'url': chrome.extension.getURL ('index.html')});
 });
