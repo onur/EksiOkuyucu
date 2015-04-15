abe39a42ecaf6b25fea7b878e629e2b1cac2acd6
Onur Aslan <onuraslan@gmail.com>
2015-04-15 11:21:25 +0300

    Parse favoriteCount and isFavorite in new theme

--- app/js/models/entry.js
+++ app/js/models/entry.js
@@ -156,11 +156,11 @@ define([
                   $(htmlDoc).find ('a.entry-date').text()));
       this.set ('id', $(htmlDoc).find ('footer').attr ('data-id'));
       this.set ('isFavorite',
-                $(htmlDoc).find ('footer').attr ('data-isfavorite') == 'true'
+                $(htmlDoc).attr ('data-isfavorite') == 'true'
                     ? true : false );
       this.set ('favoriteCount',
                 parseInt (
-                  $(htmlDoc).find ('footer').attr ('data-favorite-count'))
+                  $(htmlDoc).attr ('data-favorite-count'))
                 );
       this.set ('author', $(htmlDoc).find ('a.entry-author').text ());
       this.set ('author_tags', UserTagHelper.getUserTags(this.get('author')));
