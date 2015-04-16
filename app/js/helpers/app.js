0979f9f8be1c27058096fcff7c28918140977263
Onur Aslan <onuraslan@gmail.com>
2015-04-16 23:02:52 +0300

    Add initialize method and members

--- app/js/helpers/app.js
+++ app/js/helpers/app.js
@@ -4,5 +4,21 @@ define([
   'underscore',
   'backbone'
 ], function($, _, Backbone) {
-  return {};
+  return {
+
+    version: '',
+    isExtension: true,
+    urlPrefix: 'https://eksisozluk.com/',
+
+    initialize: function(version) {
+      this.version = version;
+
+      if (document.domain == 'eksiokuyucu.com' ||
+          document.domain.match(/github.io$/)) {
+        this.isExtension = false;
+        this.urlPrefix   = 'http://proxy.eksiokuyucu.com/';
+      }
+    }
+
+  };
 });
