c9ddfc4f30a85ce364a7f218076ebfbd42fff523
Onur Aslan <onuraslan@gmail.com>
2015-04-17 11:39:03 +0300

    Add generic modal method

--- app/js/helpers/app.js
+++ app/js/helpers/app.js
@@ -2,7 +2,19 @@
 define([
   'jquery',
   'underscore',
-  'backbone'
-], function($, _, Backbone) {
-  return {};
+  'backbone',
+  'text!templates/modal.html',
+], function($, _, Backbone, ModalTemplate) {
+  return {
+
+    modal: function(title, message) {
+
+      $('#generic-modal').remove();
+      $('body').append(_.template(ModalTemplate, { title: title,
+                                                   message: message }));
+      $('#generic-modal').modal('show');
+
+    }
+  
+  };
 });
